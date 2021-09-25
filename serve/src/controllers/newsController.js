be5ba1fe9f4ea3d0f08e0ea1523795bac93364b2
zer0fire <1076671121@qq.com>
2021-09-25 22:34:52 +0800

    fix: 修改文件到 news 关键字

--- serve/src/controllers/newsController.js
+++ serve/src/controllers/newsController.js
@@ -1,11 +1,11 @@
-const todoService = require('../services/newsService');
+const newsService = require('../services/newsService');
 
 /**
  * TodoController
  * Controller 是业务入口，由 HTTP 路由解析后调用
  * 包含待办事项的增删改查功能
  */
-class TodoController {
+class NewsController {
   /**
    * 列出所有待办事项
    * 响应格式
@@ -15,7 +15,7 @@ class TodoController {
    * @param ctx Koa 的上下文参数
    */
   async listAll(ctx) {
-    const list = await todoService.listAll();
+    const list = await newsService.listAll();
     ctx.body = {list};
   }
 
@@ -29,62 +29,25 @@ class TodoController {
    */
   async create(ctx) {
     const {title, done = false} = ctx.request.body;
-    const result = await todoService.create({title, done});
+    const result = await newsService.create({title, done});
     ctx.body = {result};
   }
+  async all(ctx) {}
+  async daily(ctx) {
+    const { date, read, like } = ctx.request.body
+    const res = await newsService.daily({ date, read, like })
+    ctx.body = {
 
-  /**
-   * 删除一条待办事项
-   * 响应格式
-   * {
-   *   ok: true
-   * }
-   * @param ctx Koa 的上下文参数
-   */
-  async delete(ctx) {
-    await todoService.delete(ctx.params.id);
-    ctx.body = {ok: true};
+    }
   }
+  async save(ctx) {
+    const { title, description, month, time } = ctx.request.body
+    const res = await newsService.save({})
+    ctx.body = {
 
-  /**
-   * 删除所有待办事项
-   * 响应格式
-   * {
-   *   ok: true
-   * }
-   * @param ctx Koa 的上下文参数
-   */
-  async deleteAll(ctx) {
-    await todoService.deleteAll();
-    ctx.body = {ok: true};
-  }
-
-  /**
-   * 将一条待办事项状态设为 done
-   * 响应格式
-   * {
-   *   ok: true
-   * }
-   * @param ctx Koa 的上下文参数
-   */
-  async done(ctx) {
-    await todoService.update(ctx.params.id, {done: true});
-    ctx.body = {ok: true};
-  }
-
-  /**
-   * 将一条待办事项状态设为 undone
-   * 响应格式
-   * {
-   *   ok: true
-   * }
-   * @param ctx Koa 的上下文参数
-   */
-  async undone(ctx) {
-    await todoService.update(ctx.params.id, {done: false});
-    ctx.body = {ok: true};
+    }
   }
 }
 
 // 导出 Controller 的实例
-module.exports = new TodoController();
+module.exports = new NewsController();
--- serve/src/routers/news.js
+++ serve/src/routers/news.js
@@ -1,18 +1,15 @@
 const Router = require('@koa/router');
 // Koa 的路由在被 use 时是无法指定前缀的, 需要在定义时就指定前缀
 const router = Router({
-  prefix: '/api/todo'
+  prefix: '/news'
 });
 
-const todoController = require('../controllers/newsController');
+const newsController = require('../controllers/newsController');
 
 // 组装路由
-router.put('/:id/done', todoController.done);
-router.put('/:id/undone', todoController.undone);
-router.get('/', todoController.listAll);
-router.post('/', todoController.create);
-router.delete('/:id', todoController.delete);
-router.delete('/', todoController.deleteAll);
+router.put('/all', newsController.all);
+router.put('/daily', newsController.daily);
+router.get('/save', newsController.save);
 
 // Koa 的路由需要调用 routes 函数获取实际用于 use 的函数
 module.exports = router.routes();
