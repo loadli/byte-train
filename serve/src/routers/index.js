0181544c3bcae531a02bbf47df03950a6df19e5e
黄晓林 <wx0huangxl@inspur.com>
2021-09-26 15:10:50 +0800

    feat: 完成首页数据渲染

--- serve/src/routers/index.js
+++ serve/src/routers/index.js
@@ -1,20 +1,27 @@
-var router = new (require('koa-router'))();
-const newsService = require('../services/newsService');
+var router = new (require('koa-router'))()
+const newsService = require('../services/newsService')
+const dayjs = require('dayjs')
 
-router.get('/', function *(next) {
+router.get('/', function* (next) {
   const data = yield newsService.all()
+
+  data.map((item) => {
+    item.time = dayjs(item.time).format('YYYY-MM-DD')
+    return item
+  })
+
   yield this.render('index', {
     title: '掘金前端日报',
     data,
-  });
-});
+  })
+})
 
-router.get('/news', function *(next) {
+router.get('/news', function* (next) {
   const data = newsService.daily()
   yield this.render('news', {
     title: '掘金前端日报',
     data,
-  });
-});
+  })
+})
 
-module.exports = router;
+module.exports = router
